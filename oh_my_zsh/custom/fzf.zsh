eb9e0dbabd5c2642474ab03ac6006438a1cee204
Hunter Braun <hunter.braun@gmail.com>
2015-10-23 16:14:58 -0500

    add fzf-cd-widget binding

--- oh_my_zsh/custom/fzf.zsh
+++ oh_my_zsh/custom/fzf.zsh
@@ -1,7 +1,8 @@
 export FZF_DEFAULT_COMMAND='ag -l -g ""'
 
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
 export FZF_DEFAULT_OPTS='
---extended
 --bind ctrl-f:page-down,ctrl-b:page-up
 --ansi
 --color fg:-1,bg:-1,hl:-1,fg+:-1,bg+:-1,hl+:-1
@@ -9,4 +10,5 @@ export FZF_DEFAULT_OPTS='
 '
 
 bindkey '^P' fzf-file-widget
-bindkey '^F' fzf-history-widget
+bindkey '^H' fzf-history-widget
+bindkey '^D' fzf-cd-widget
