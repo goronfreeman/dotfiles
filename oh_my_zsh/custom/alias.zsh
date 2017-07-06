30de3cb688d1702164fb4c0d207129f66521129b
Hunter Braun <hunter.braun@gmail.com>
2017-07-06 16:47:03 -0500

    Update aliases

--- oh_my_zsh/custom/alias.zsh
+++ oh_my_zsh/custom/alias.zsh
@@ -1,9 +1,12 @@
 # Symlink path:
 #   ln -s ~/github/dotfiles/oh_my_zsh/.aliasrc ~
 
+# cd
+alias ..="cd .."
+alias ...="cd ../.."
+
 # git
 alias g="git"
-
 alias ga="g add"
 alias gb="gco -b"
 alias gc="g commit -m"
@@ -18,27 +21,17 @@ alias gst="g stash"
 alias gup="g pull --rebase"
 alias gl="g log"
 
+# bundler
+alias be="bundle exec"
+alias ber="be rake"
+alias rr="rake routes"
+
 # rails
 alias rc="be bin/rails c"
 alias rs="be bin/rails s"
 alias rg="bin/rails generate"
 alias rgm="rg migration"
 alias rgc="rg controller"
-
-# bundler
-alias be="bundle exec"
-alias ber="be rake"
-alias rr="rake routes"
-
-# homebrew
-alias brewup="brew update"
-alias brewup="brew update && brew upgrade --cleanup && brew cleanup -s && brew prune && brew cask cleanup"
-
-# bower
-alias bower='noglob bower'
-
-# python
-alias pyserv="python -m SimpleHTTPServer"
 
 # database
 alias dbd="ber db:drop"
@@ -47,20 +40,28 @@ alias dbm="ber db:migrate"
 alias dbs="ber db:seed"
 alias dbrb="dbd && dbc && dbm"
 
-# other
+# homebrew
+alias brewup="brew update && brew upgrade --cleanup && brew cleanup -s && brew prune && brew cask cleanup"
+
+# neovim
+alias vim="nvim"
+
+# docker
+alias dc="docker-compose"
+
+# foreman
+alias fs="foreman start"
+
+# bower
+alias bower='noglob bower'
+
+# python
+alias pyserv="python -m SimpleHTTPServer"
+
+# misc
+alias ll="ls -lah"
 alias multizip="zip -r -s 3g archive.zip" #foldername
 alias annotate="annotate --position bottom --exclude tests,fixtures,factories,serializers"
 alias kbdisable="sudo kextunload /System/Library/Extensions/AppleUSBTopCase.kext/Contents/PlugIns/AppleUSBTCKeyboard.kext/ 2>/dev/null"
 alias kbenable="sudo kextload /System/Library/Extensions/AppleUSBTopCase.kext/Contents/PlugIns/AppleUSBTCKeyboard.kext/"
 alias weather="curl "http://wttr.in/${1-}""
-
-# neovim
-alias vim="nvim"
-
-# vagrant
-alias vu="vagrant up && vagrant ssh"
-alias vh="vagrant halt"
-alias vssh="vagrant ssh"
-
-# docker
-alias dc="docker-compose"
