a4d70aaaaa23d75b063d3ceeb16f1042a54be494
Hunter Braun <hunter.braun@gmail.com>
2018-06-19 01:21:06 -0500

    Modify some functions and add documentation

--- oh_my_zsh/custom/fzf_git.zsh
+++ oh_my_zsh/custom/fzf_git.zsh
@@ -5,6 +5,11 @@ is_in_git_repo() {
   git rev-parse HEAD > /dev/null 2>&1
 }
 
+# git add
+# List all modified files that have not been staged
+# The preview window shows a diff of the file against the index
+# ctrl-g ctrl+a brings up the viewer
+# enter will stage the selected file
 git_a() {
   if is_in_git_repo; then
     local file=$(git -c color.status=always status --short |
@@ -18,11 +23,16 @@ git_a() {
   fi
 }
 
+# git branch
+# List all branches for the current repository
+# The preview window shows the branch structure
+# ctrl-g ctrl+b brings up the viewer
+# enter will checkout the selected branch
 git_b() {
   if is_in_git_repo; then
     local branch=$(git branch -a --color=always | grep -v '/HEAD\s' | sort |
       fzf-tmux --ansi --multi --tac --preview-window right:70% \
-      --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) | head -'$LINES |
+      --preview 'git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always | head -'$LINES |
       sed 's/^..//' | cut -d' ' -f1 |
       sed 's#^remotes/##')
 
@@ -32,15 +42,29 @@ git_b() {
   fi
 }
 
+# git files
+# List all modified files that have not been staged
+# The preview window shows a diff of the file against the index
+# ctrl-g ctrl+f brings up the viewer
+# enter will checkout the selected file
 git_f() {
   if is_in_git_repo; then
-    git -c color.status=always status --short |
-      fzf-tmux -m --ansi --nth 2..,.. \
+    local file=$(git -c color.status=always status --short |
+      fzf-tmux --multi --preview-window right:70% --ansi --nth 2..,.. \
       --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1}) | head -500' |
-      cut -c4- | sed 's/.* -> //'
+      cut -c4- | sed 's/.* -> //')
+
+    if [[ $file ]]; then
+      git checkout --quiet $file
+    fi
   fi
 }
 
+# git hashes
+# List all modified files that have not been staged
+# The preview window shows a diff of the file against the index
+# ctrl-g ctrl+h brings up the viewer
+# enter will print out the commit ID
 git_h() {
   if is_in_git_repo; then
     git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
@@ -51,11 +75,16 @@ git_h() {
   fi
 }
 
+# git reset
+# List all modified files that have not been staged
+# The preview window shows the content of the selected file
+# ctrl-g ctrl+r brings up the viewer
+# enter will print out the commit ID
 git_r() {
   if is_in_git_repo; then
     local file=$(git diff --name-only --cached --color=always |
       fzf-tmux --multi --preview-window right:70% \
-      --preview 'cat {} | head -'$LINES)
+      --preview '(coderay {} || cat {}) | head -'$LINES)
 
     if [[ $file ]]; then
       git reset --quiet $file
@@ -63,6 +92,10 @@ git_r() {
   fi
 }
 
+# git tags
+# List all tags for the current repository
+# ctrl-g ctrl+t brings up the viewer
+# enter will checkout the selected tag
 git_t() {
   if is_in_git_repo; then
     local tag=$(git tag --sort -version:refname |
