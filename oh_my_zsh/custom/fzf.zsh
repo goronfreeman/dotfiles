91b3a18ff0fb4f00381d74cfd39ade659d9bef52
Hunter Braun <hunter.braun@gmail.com>
2018-07-11 15:12:32 -0500

    Update colors for nord colorscheme

--- oh_my_zsh/custom/fzf.zsh
+++ oh_my_zsh/custom/fzf.zsh
@@ -5,8 +5,9 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_DEFAULT_OPTS='
 --bind ctrl-f:page-down,ctrl-b:page-up
 --ansi
---color fg:-1,bg:-1,hl:-1,fg+:-1,bg+:-1,hl+:-1
---color info:-1,prompt:-1,spinner:-1,pointer:-1,marker:-1
+--color fg:-1,bg:-1,hl:#81A1C1
+--color fg+:-1,bg+:-1,hl+:#8FBCBB
+--color info:-1,prompt:#A3BE8C,pointer:#8FBCBB,marker:#EBCB8B,spinner:#EBCB8B,header:-1
 '
 
 bindkey '^P' fzf-file-widget
